93b472b41b042082a7853e40600d9eaceb7540ea
kumarnitish378 <41774164+kumarnitish378@users.noreply.github.com>
2019-09-25 06:49:11 -0700

    Add files via upload

---
+++ Turtle Path.py
@@ -0,0 +1,36 @@
+from turtle import *
+speed(10)
+a="red"
+pensize(3)
+for i in range(72):
+      if (a == "red"):
+            a="green"
+      else:
+            a="red"
+      color(a)
+      forward(50)
+      left(30)
+      forward(30)
+      right(90-30)
+      left(60)
+      right(30)
+      forward(50)
+      right(30)
+      forward(30)
+      left(30)
+      forward(50)
+      left(90)
+      forward(100)
+      left(90)
+      forward(50)
+      left(30)
+      forward(30)
+      right(30)
+      forward(50)
+      right (30)
+      forward(30)
+      left(30)
+      forward(50)
+      left(90)
+      forward(100)
+      left(5)
---
+++ circle rangoli.py
@@ -0,0 +1,28 @@
+import turtle as t
+t.bgcolor("#125364")
+t.color("#125364")
+t.goto(0,100)
+t.pensize(2)
+t.speed(9)
+t.color("green")
+for i in range (0,75,1):
+    t.forward(300)
+    t.left(145)
+t.right(225)
+t.color("#125364")
+t.forward(50)
+t.color("red")
+for i in range (0,75,1):
+    t.forward(300)
+    t.left(145)
+t.right(150)
+t.color("#125364")
+t.forward(50)
+t.color("dark blue")
+for i in range (0,75,1):
+    t.forward(300)
+    t.left(145)
+
+
+
+
